806ebfb84c28c05a8df8545f61a73cd99fc7508b
softwareengineer012az <softwareengineer012az@gmail.com>
2024-05-25 08:16:55 +0000

    Implement initial files strucure finally

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+export const dynamic = 'force-dynamic'
+
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "../globals.css";
+
+
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+   <main>
+    SIDEBAR
+    {children}
+   </main>
+  );
+}
